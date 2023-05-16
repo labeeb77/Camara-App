18ff2383b774fda910c1c0e92984e66f9fe4c0ff
Mohammed Labeeb <labeebpktr77@gmail.com>
2023-05-16 13:22:47 +0530

    camara App

---
+++ lib/functions/read_gallery.dart
@@ -0,0 +1,12 @@
+
+import 'dart:io';
+
+List readGallery() {
+  List fileList =
+      Directory("/storage/emulated/0/Pictures/Flutter Camera/").listSync();
+
+  return fileList;
+  // for (int index = 0; index < fileList.length; index++) {
+  //   log("The File:::${fileList[index]}");
+  // }
+}
